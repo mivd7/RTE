2d9363b1401db9bbab326dd0538f87cf45433b2a
mivd7 <mi.vandeurzen@gmail.com>
2019-12-13 15:14:37 +0100

    move js to seperate file

---
+++ index.js
@@ -0,0 +1,34 @@
+DecoupledEditor
+            .create(document.querySelector('#editor'), {
+                fontSize: {
+                    options: [
+                        10,
+                        12,
+                        14,
+                        'default',
+                        18,
+                        20,
+                        22,
+                        24,
+                        26,
+                        28,
+                        32,
+                        36,
+                        40
+                    ]
+                },
+            })
+            .then(editor => {
+                const toolbarContainer = document.querySelector('#toolbar-container');
+
+                toolbarContainer.appendChild(editor.ui.view.toolbar.element);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+        function handleClick() {
+            var editorInput = document.getElementById('editor')
+            var output = document.getElementById('output');
+            output.insertAdjacentText("beforebegin", editorInput.innerHTML)
+        }
