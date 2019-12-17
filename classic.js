79b3ba44a18d0127028aa6c0366388059e7901fb
mivd7 <mi.vandeurzen@gmail.com>
2019-12-17 10:08:14 +0100

    inline script classiceditor

---
+++ classic.js
@@ -0,0 +1,11 @@
+let editor;
+
+ClassicEditor
+    .create(document.querySelector('#editor'))
+    .then(newEditor => {
+        editor = newEditor;
+
+    })
+    .catch(error => {
+        console.error(error);
+    });
